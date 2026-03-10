05e33e45cc80d1c4de7faf5e7a2b7521e58be1ca
Surbhi Rawat <shurbhirawat43@gmail.com>
2026-03-10 23:42:55 +0530

    forms

--- src/AllTopics/08_Forms/ControlledForm2.jsx
+++ src/AllTopics/08_Forms/ControlledForm2.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 const ControlledForm2 = () => {
   const [formData, setFormData] = useState({
@@ -6,22 +6,76 @@ const ControlledForm2 = () => {
     email: "",
     password: "",
   });
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    //  console.log(name, value);
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+    console.log(formData);
+
+    //! to get existing user from local storage, if user are present Parse it else initialize with empty array []
+    let users = JSON.parse(localStorage.getItem("users")) || []
+
+
+    //! Create user data with id 
+    let userData = {...formData, id: Math.random()};
+
+    //! add userData to user Array
+    users.push(userData);
+
+    //! set users array to local storage after converting into json
+    localStorage.setItem("users",JSON.stringify(users));
+
+    // clear input fields
+    setFormData({username: "",email: "", password: ""})
+  };
+
   return (
     <div>
-        <h1>Learn Controlled Forms</h1>
-        <p>Manage multiple input with single state</p>
+      <h1>Learn Controlled Forms</h1>
+      <p>Manage multiple input with single state</p>
 
-        <form>
-          <input type="text" placeholder='Enter Username' value={formData.username}/>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
 
-          <br /><br />
+        <br />
+        <br />
 
-          <input type="text" placeholder='Enter Email' value={formData.email}/>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
 
-          <br /><br />
+        <br />
+        <br />
 
-          <input type="text" placeholder='Enter Password' value={formData.password}/>
-        </form>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
