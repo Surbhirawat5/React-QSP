24629974a1792e5ef93806ea651eae5fe767656e
Surbhi Rawat <shurbhirawat43@gmail.com>
2026-03-20 12:32:09 +0530

    update router

--- src/AllTopics/16_Routing/Nav.jsx
+++ src/AllTopics/16_Routing/Nav.jsx
@@ -16,15 +16,24 @@
 
 //! USING ROUTING - REACT ROUTER DOM
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <header className='p-5 bg-violet-600 text-white font-bold'>
-      <nav className='flex gap-10'>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-     </nav>
+    <header className="p-5 bg-violet-600 text-white font-bold">
+      <nav className="flex gap-10">
+        {/* <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link> */}
+
+        <NavLink
+          className={({ isActive }) =>`${isActive && "bg-white text-black"} transition ease-in py-1 px-3 rounded`} to="/">
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>`${isActive && "bg-white text-black"} transition ease-in py-1 px-3 rounded`} to="/about">
+          About
+        </NavLink>
+      </nav>
     </header>
   );
 };
