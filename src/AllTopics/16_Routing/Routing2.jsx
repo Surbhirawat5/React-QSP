6c5186f82e31b007d43f202b8cae95cf93ab48b1
Surbhi Rawat <shurbhirawat43@gmail.com>
2026-03-23 12:30:35 +0530

    Layout

--- src/AllTopics/16_Routing/Routing2.jsx
+++ src/AllTopics/16_Routing/Routing2.jsx
@@ -1,12 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Layout from "./Layout";
 
 
+const myRoutes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
 
+            {
+                path: "/about",
+                element: <About/>
+            },
 
-
+            {
+                path: "/contact",
+                element: <Contact/>
+            },
+        ],
+    },
+]);
 
 
 const Routing2 = () => {
-  return 
-}
+  return <RouterProvider router={myRoutes} />
+};
 
 export default Routing2;
