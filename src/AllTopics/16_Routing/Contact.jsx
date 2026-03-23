a2169e98484bc28fc999d764499733890d11a201
Surbhi Rawat <shurbhirawat43@gmail.com>
2026-03-23 23:33:58 +0530

    updated

--- src/AllTopics/16_Routing/Contact.jsx
+++ src/AllTopics/16_Routing/Contact.jsx
@@ -6,8 +6,9 @@ const Contact = () => {
         <h1 className='text-center font-bold text-5xl p-8'>Contact</h1>
         
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cumque praesentium corrupti tempora ex architecto ab accusamus dolore odio exercitationem, repudiandae voluptatum tenetur nihil natus omnis. Tempora ab nesciunt amet adipisci quidem dolore ducimus quia aspernatur. Qui quidem excepturi veniam incidunt iusto quaerat.</p>
+
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
