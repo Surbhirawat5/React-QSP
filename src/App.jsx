e4ae97a320639937149ac71c813e672ceb8c7fab
Surbhi Rawat <shurbhirawat43@gmail.com>
2026-03-26 21:12:21 +0530

    CrudApp Day 1

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,7 @@ import Dashboard from "./AllTopics/16_Routing/Dashboard";
 import ParentHoc from "./AllTopics/17_Hoc/ParentHoc";
 import Routing2 from "./AllTopics/16_Routing/Routing2";
 import ReduxCounter from "./AllTopics/18_Redux/ReduxCounter";
+import VirtualisedList from "./AllTopics/19_Virtulization/VirtualisedList";
 
 const App = () => {
 
@@ -135,7 +136,9 @@ const App = () => {
 
       {/* <ParentHoc/>   */}
 
-      <ReduxCounter/>
+      {/* <ReduxCounter/> */}
+
+      <VirtualisedList/>
 
     </>
   );
