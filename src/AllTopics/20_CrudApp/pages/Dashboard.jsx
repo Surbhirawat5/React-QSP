ee9d035e47787923828f7e8ad124db5ac197bc4a
Surbhi Rawat <shurbhirawat43@gmail.com>
2026-03-30 23:06:14 +0530

    update : sorting, Searching and display from API in table form

--- src/AllTopics/20_CrudApp/pages/Dashboard.jsx
+++ src/AllTopics/20_CrudApp/pages/Dashboard.jsx
@@ -2,11 +2,26 @@ import React, { useState } from 'react'
 import SearchBar from '../components/SearchBar';
 import Sorting from '../components/Sorting';
 import Pagination from '../components/Pagination';
+import { usePosts } from '../hooks/usePost';
+import { useDebounce } from '../hooks/useDebounce';
 
 const Dashboard = () => {
-  const [query,setQuery] = useState("");
-  const [sortBy, setSortBy] = useState("");
+  const [query,setQuery] = useState(""); // use for searching
+  const [sortBy, setSortBy] = useState(""); // sorting 
   const [skip, setSkip] = useState(0);
+
+
+
+// ! debounced query
+let debouncedQuery = useDebounce(query,2000)
+
+  //! Calling custom hook :-
+
+  const { posts,loading} = usePosts({query: debouncedQuery, sortBy, skip});
+  // const { posts,loading} = usePosts({query, sortBy, skip});
+
+
+
   return (
     <section className='pt-20 px-10'>
       <header className='font-bold my-5'>
@@ -20,12 +35,43 @@ const Dashboard = () => {
       <Sorting sortBy={sortBy} setSortBy={setSortBy}/>
 
       {/* DISPLAY POSTS FROM API */}
+      {loading ? <p> Loading...</p> : <>
 
+      <table>
+        <thead className='bg-gray-400'>
+          <tr>
+            <th className='p-2'>ID</th>
+            <th className='p-2'>TITLE</th>
+            <th className='p-2'>VIEWS</th>
+          </tr>
+        </thead>
 
+        <tbody>
+          {posts.map((item) =>{
+            return(
+              <tr key={item.id} className='hover:bg-gray-100 cursor-pointer'>
+                <td className='border border-gray-300 p-2'>{item.id}</td>
+                <td className='border border-gray-300 p-2'>{item.title}</td>
+                <td className='border border-gray-300 p-2'>{item.views}</td>
+              </tr>
+            )
+      })}
 
+        </tbody>
+      </table>
+
+      
+      
+      
       {/* PAGINATION */}
       <Pagination skip={skip} setSkip={setSkip}/>
 
+      
+      </>}
+
+
+
+      
 
     </section>
   );
